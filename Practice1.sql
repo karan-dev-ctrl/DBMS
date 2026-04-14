f85f63ce0e9163d60e41818f3455c22009f60614
Karan <karanpa3591@gmail.com>
2026-04-14 12:46:07 +0200

    Add files via upload

--- Practice1.sql
+++ Practice1.sql
@@ -145,3 +145,204 @@ from "Order" o
 join OrderItem oi on oi.ido = o.ido
 group by o.idc;
 
+
+select distinct * 
+from "Order"
+where Order_datetime between '2000-01-01' and '2023-01-01'
+order by order_datetime asc;
+
+--List all orders between 2000 and 2023
+
+select * from "Order"
+where order_datetime between '2000' and '2023'
+order by order_datetime asc;
+
+--List all products with price greater than 10,000.
+
+select * from Product
+where unit_price > 10000;
+
+--List all order items where quantity > 5
+
+select * from OrderItem
+where quantity > 5;
+
+--Show product name and quantity from orders.
+
+select p.name, oi.quantity
+from Product p
+join OrderItem oi on p.idp = oi.idp;
+
+--List customer id and product id for all orders.
+select o.idc, oi.idp
+from "Order" o
+join OrderItem oi on o.ido = oi.ido
+
+--List product name and order item price.
+
+select p.name, oi.unit_price
+from "Product" p
+join OrderItem oi on p.idp = oi.idp;
+
+
+--- Count how many order items exist
+select count(*) as cnt
+from OrderItem
+
+
+--Find total quantity ordered for each product
+
+select oi.idp, sum(oi.quantity) as total_quantity
+from "OrderItem" oi
+group by oi.idp
+
+--Find maximum order item price per product.
+select oi.idp, max(oi.unit_price) as max
+from "OrderItem" oi
+group by oi.idp
+
+--Find average order item price per product.
+select oi.idp, avg(oi.unit_price) as avg
+from OrderItem oi
+group by oi.idp;
+
+--total sales per customer
+
+select o.idc, sum(oi.unit_price * oi.quantity) as total_sales
+from "Order" o
+join OrderItem oi on o.ido = oi.ido
+group by o.idc;
+
+---products ordered more than 3 times
+
+
+
+--Find customers whose total sales > 50,000
+
+select o.idc, sum(oi.unit_price  * oi.quantity) as total_Sales_cust
+from "Order" o
+join OrderItem oi on o.ido = oi.ido
+group by o.idc
+having sum(oi.unit_price  * oi.quantity) > 50000
+
+--Show first 5 products
+
+select top 3 * 
+from Product 
+order by unit_price desc
+
+---Top 5 most expensive products
+select top 5 * 
+from Product 
+order by unit_price asc
+
+----Top 5 order items with highest quantity
+select top 5 *
+from OrderItem oi
+order by oi.quantity asc
+
+---Top 5 products by total quantity ordered
+--| Phrase            | Use    |
+--| ----------------- | ------ |
+--| Top N             | `DESC` |
+--| Lowest / Cheapest | `ASC`  |
+
+select top 5 oi.idp, sum(oi.quantity) as total_quantity
+from OrderItem oi 
+group by oi.idp
+order by total_quantity desc
+
+--Top 3 customers by total sales
+
+select top 3 o.idc, sum(oi.unit_price * oi.quantity) as total_sales
+from "Order" o 
+join OrderItem oi on o.ido = oi.ido
+group by o.idc 
+order by total_sales desc
+
+---Top 3 customers whose total sales > 50,000
+
+select top 3 o.idc, sum(oi.unit_price * oi.quantity) as total_sales
+from "Order" o 
+join OrderItem oi on o.ido = oi.ido
+group by o.idc
+having sum(oi.unit_price * oi.quantity) > 50000
+order by total_sales desc
+
+
+---query 1
+
+select count(*) as record,count(distinct o.idc ) as cnt
+from "Order" o
+join OrderItem oi on o.ido = oi.ido
+join Product p on p.idp = oi.idp
+where oi.quantity  > 5 and
+p.unit_price > 10000
+
+
+--query 2
+select distinct o.idc as cust
+from "Order" o 
+join OrderItem oi on o.ido = oi.ido
+join Product p on p.idp = oi.idp
+where p.unit_price between 8000 and 9000;
+
+---query 3
+select oi.idp, sum(oi.unit_price * oi.quantity) as total_quantity
+from OrderItem oi
+group by oi.idp
+
+---query 4
+select oi.idp, max(oi.unit_price) as max
+from OrderItem oi
+group by oi.idp
+
+--query 5 
+select oi.idp, avg(oi.unit_price) as max
+from OrderItem oi
+group by oi.idp
+
+
+
+--query 20,
+
+select o.idc as cust, sum(oi.unit_price * oi.quantity) as total_sales 
+from "Order" o 
+join OrderItem oi on o.ido = oi.ido
+join Product p on p.idp = oi.idp
+where p.unit_price > 10000 and 
+oi.quantity > 5 
+group by o.idc 
+having sum(oi.unit_price * oi.quantity) > 50000;
+
+--query 12 
+
+select top 3 o.idc as top_3_cust,
+       sum(oi.unit_price * oi.quantity) as total_sales
+from "Order" o
+join OrderItem oi on o.ido = oi.ido
+join Product p on p.idp = oi.idp
+group by o.idc
+order by total_sales desc;
+
+---query 8
+
+select oi.idp as pid, count (*)  as no_of_order_items
+from OrderItem oi
+group by oi.idp
+having count (*)  > 3
+
+--query 6 
+
+select o.idc as cust, sum(oi.unit_price * oi.quantity) as total_sales
+from "Order" o 
+join OrderItem oi on o.ido = oi.ido
+group by o.idc
+
+---Top 5 products by total sales (slightly tricky)
+
+select top 5 p.name, sum(oi.unit_price * oi.quantity) as total_sales
+from Product p 
+join OrderItem oi on p.idp = oi.idp
+group by p.name
+order by total_sales desc
